e470469b3eb723e842e428b5a331ff847c26958c
MINSK\Yuliya_Kharytaniuk <nevie15@gmail.com>
2018-11-23 11:27:08 +0300

    css added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const css= require("./styles/style.css");
 import {Controller} from "./controllers/Controller.js";
 let newsController = new Controller(document.getElementsByTagName("section")[0]);
 newsController.getNewsChannels();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-require("babel-register");
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
 
 const config = {
-
-    entry: ['babel-polyfill','whatwg-fetch', "./src/app.js"],
+    entry: ['babel-polyfill', 'whatwg-fetch', "./src/app.js"],
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js',
@@ -19,14 +19,37 @@ const config = {
                     loader: 'babel-loader'
                 }],
             },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: ExtractTextWebpackPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        {
+                            loader: 'css-loader'
+                        }],
+                    publicPath:'/dist'
+                }),
+            }
         ]
     },
-  plugins : [
-       new webpack.ProvidePlugin({
-           Promise: 'exports-loader?self.Promise!es6-promise/dist/es6-promise',
-           fetch: 'exports-loader?self.fetch!whatwg-fetch/dist/fetch.umd',       }),
+
+    plugins: [
+        new webpack.ProvidePlugin({
+            Promise: 'exports-loader?self.Promise!es6-promise/dist/es6-promise',
+            fetch: 'exports-loader?self.fetch!whatwg-fetch/dist/fetch.umd',
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            minify:{
+                collapseWhitespace:true
+            },
+            hash: true,
+            template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+        }),
+        new ExtractTextWebpackPlugin('style.css')
+
     ],
     watch: true,
 };
-// Exports
 module.exports = config;
